c0a3d82f7765d027357722656bd1d6e576d04386
FionaYHWu <yhw3@uw.edu>
2021-03-16 16:41:11 -0700

    updated testing

--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,3 +1,6 @@
 test_that("non-data-frame input throws error", {
   expect_error(my_lm("a string", "throws errors"))
 })
+test_that("output is a list", {
+  expect_is(my_lm(body_mass_g ~ bill_length_mm, data = my_penguins), "list")
+})
--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -1,3 +1,7 @@
 test_that("throws error if there's only one param", {
   expect_error(my_t.test(2))
 })
+
+test_that("output is a list", {
+  expect_is(my_t.test(rnorm(100), 0, "greater"), "list")
+})
