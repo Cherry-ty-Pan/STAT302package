80c0c03566781fdb9eac5c1a00f4ff9f539bd5ad
FionaYHWu <yhw3@uw.edu>
2021-03-17 22:30:40 -0700

    edited

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -14,13 +14,12 @@
 #'
 #' @export
 my_rf_cv <- function(k) {
-  data(my_penguins)
   train <- my_penguins[,3:6]
   # Create fold
   fold <- sample(rep(1:k, length = nrow(train)))
   # Create the data set for model
   train <- data.frame("x" = train[,1:3],
-                      "cl" = train[,4],
+                      "cl" = train$body_mass_g,
                       "split" = fold)
   # Remove NA value
   train <- na.omit(train)
@@ -34,11 +33,11 @@ my_rf_cv <- function(k) {
       dplyr::filter(split == i)
     # Train a random forest model with 100 trees to predict body_mass_g using
     # covariates bill_length_mm, bill_depth_mm, and flipper_length_mm
-    random_model <- randomForest::randomForest(body_mass_g ~ x.bill_length_mm + x.bill_depth_mm + x.flipper_length_mm, data = train_data, ntree = 100)
+    random_model <- randomForest::randomForest(cl ~ x.bill_length_mm + x.bill_depth_mm + x.flipper_length_mm, data = train_data, ntree = 100)
     # predict the body_mass_g of the ith fold which was not used as training data
     prediction <- predict(random_model, test_data[, -4])
     # evaluate the MSE
-    mse <- mean((prediction - test_data$body_mass_g)^2)
+    mse <- mean((prediction - test_data$cl)^2)
   }
   # Return the average MSE across all k folds
   mse <- mean(mse)
